269303b082c5416483d6ef1f9a00e9f53b0118e3
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-24 14:54:39 +0300

    order items models

---
+++ Models/Orderitems.cs
@@ -0,0 +1,12 @@
+﻿namespace EMedicineBE.Models
+{
+    public class OrderItems
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int MedicineId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
