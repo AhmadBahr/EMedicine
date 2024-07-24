d624776dc75c0908dc3529131dc1d11db57e40cc
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-24 22:55:13 +0300

    add to cart

--- Controllers/MedicinesController.cs
+++ Controllers/MedicinesController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Net.NetworkInformation;
+using EMedicineBE.Models;
+using System.Data.SqlClient;
 
 namespace EMedicineBE.Controllers
 {
@@ -7,5 +11,20 @@ namespace EMedicineBE.Controllers
     [ApiController]
     public class MedicinesController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public MedicinesController(IConfiguration configuration)
+        {
+            _configuration = _configuration;
+        }
+        [HttpPost]
+        [Route("addToCart")]
+        public Response addToCart(Cart cart)
+        {
+            DAL dal = new DAL();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("EMedCS").ToString());
+            Response response = dal.addToCart(cart, connection);
+            return response;
+        }
     }
 }
