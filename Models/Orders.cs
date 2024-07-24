9d0d6adeb46f0f543ddb6f7fb759716e7ca23c9f
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-24 14:36:47 +0300

    adding orders model

--- Models/Orders.cs
+++ Models/Orders.cs
@@ -2,5 +2,10 @@
 {
     public class Orders
     {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string OrderNo { get; set; }
+        public decimal OrderTotal { get; set; }
+        public string OrderStatus { get; set; }
     }
 }
