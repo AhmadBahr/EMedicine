51fb1e0a80e440bd6d46909c15891256c53a1444
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-24 16:12:35 +0300

    cart model

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -2,5 +2,12 @@
 {
     public class Cart
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MedicineID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
     }
 }
