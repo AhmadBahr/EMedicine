06fb2f51ce960052fa7a508cb85a768fa46c829d
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-24 16:31:07 +0300

    dal login

--- Models/DAL.cs
+++ Models/DAL.cs
@@ -45,3 +45,51 @@ namespace EMedicineBE.Models
             }
             return response;
         }
+    }
+    public Response login(Users users, SqlConnection connection)
+    {
+        Response response = new Response();
+        try
+        {
+            using (SqlCommand cmd = new SqlCommand("sp_login", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Email", users.Email);
+                cmd.Parameters.AddWithValue("@Password", users.Password);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    DataRow row = dt.Rows[0];
+                    Users user = new Users
+                    {
+                        ID = Convert.ToInt32(row["ID"]),
+                        FirstName = Convert.ToString(row["FirstName"]),
+                        LastName = Convert.ToString(row["LastName"]),
+                        Email = Convert.ToString(row["Email"]),
+                        Type = Convert.ToString(row["Type"])  // Ensure this field is correctly set
+                    };
+
+                    response.StatusCode = 200;
+                    response.StatusMessage = "User logged in successfully";
+                    response.user = user;
+                }
+                else
+                {
+                    response.StatusCode = 100;
+                    response.StatusMessage = "Invalid email or password";
+                    response.user = null;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            response.StatusCode = 500;
+            response.StatusMessage = $"Exception: {ex.Message}";
+        }
+        return response;
+    }
+}
