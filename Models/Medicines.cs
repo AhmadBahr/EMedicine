b5582a9a43fad1e106d0ab58a706f2c1401d0fe4
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-07-24 15:48:15 +0300

    medicines model

--- Models/Medicines.cs
+++ Models/Medicines.cs
@@ -2,5 +2,16 @@
 {
     public class Medicines
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ExpDate { get; set; }
+        public string ImageUrl { get; set; }
+        public int Status { get; set; }
+        public decimal Discount { get; set; }
+        public string Type { get; set; }
     }
 }
